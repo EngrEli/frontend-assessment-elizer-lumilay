415df3a9abd13e8a02cfe7b2ed208e35299c703f
unknown <elizerlumilay@gmail.com>
2024-07-12 23:46:57 +0800

    Adjust main hero text responsiveness

--- exercise-1/src/components/MainHero.vue
+++ exercise-1/src/components/MainHero.vue
@@ -3,10 +3,14 @@
         class="w-full px-8 md:px-0 bg-[url('https://via.placeholder.com/600x600')] md:bg-[url('https://via.placeholder.com/1920x650')] bg-blend-darken bg-[#0005] text-white flex flex-col justify-center align-center h-[650px] bg-cover bg-center bg-no-repeat"
     >
         <div class="max-w-7xl m-auto text-center">
-            <h1 class="text-7xl font-bold leading-relaxed md:leading-normal">
+            <h1
+                class="text-5xl md:text-7xl font-bold leading-relaxed md:leading-normal"
+            >
                 Hello Developer!
             </h1>
-            <h3 class="text-4xl mt-2 leading-relaxed md:leading-normal">
+            <h3
+                class="text-2xl md:text-4xl mt-2 leading-relaxed md:leading-normal"
+            >
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit
             </h3>
         </div>
